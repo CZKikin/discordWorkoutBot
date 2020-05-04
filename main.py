b4585c14ad651e6e9e6d0b6164362bda8914484f
Kikin <56359940+CZKikin@users.noreply.github.com>
2020-05-04 21:20:53 +0200

    Develop (#2)
    * Hidden old token
    
    * new functionality

--- main.py
+++ main.py
@@ -19,7 +19,21 @@ async def ping(ctx):
     await ctx.send(f"Tvá odezva je: {client.latency * 1000}ms, Tvůj workoutbot_beta :)")
 
 @client.command()
-async def vojta(ctx):
-    await ctx.send("Vojta je prostě machr :lidl:")
-
+async def beh(ctx, distance, time):
+    try:
+        distance = float(distance)
+    except ValueError:
+        await ctx.send("Zadal jsi špatně dráhu - zadej číslo v km")
+        return
+    
+    await ctx.send(f'Uběhl jsi {distance} za {time}')
+    
+@client.command()
+async def hilfe(ctx):
+    await ctx.send('''Tož co chceš...
+Ping - Vypíše ping.
+Beh dráha čas - zapíše běh.
+Na dalších commandech se pracuje. :)
+    ''')
 client.run("TOKEN_HERE")
+
