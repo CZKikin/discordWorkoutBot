ed3783e135dc5a53fbf7337f24487fbe401fe6e2
CZKikin <krystofsadlik13@seznam.cz>
2020-05-06 11:38:28 +0200

    Input checks and author identification

--- main.py
+++ main.py
@@ -22,18 +22,41 @@ async def ping(ctx):
 async def beh(ctx, distance, time):
     try:
         distance = float(distance)
+        minutes, seconds = time.split(":")    
+
     except ValueError:
         await ctx.send("Zadal jsi špatně dráhu - zadej číslo v km")
         return
     
-    await ctx.send(f'Uběhl jsi {distance} za {time}')
+    except Exception as exx:
+        await ctx.send("Nastala chyba bota - kontaktuj admina :)")
+        print(f"ERROR WITH ARGUMENTS distance = {distance} time = {time}\n{exx}")
+        return
+
+    try:
+        minutes = int(minutes)
+        seconds = int(seconds)
+        if seconds > 59 or seconds < 0 or minutes < 0:
+            await ctx.send("Špatně zadaný čas X:XX")
+            return
+    except ValueError:
+        await ctx.send("Zadal jsi špatně čas!")
+        return
+
+    print(minutes, seconds)
+    author = str(ctx.author)
+    author = author.split('#') 
+    await ctx.send(f"Uběhl jsi {distance}km za {minutes} minut a {seconds} sekund, jen tak dál {author[0]}.")
     
+async def save_table():
+    pass
+
 @client.command()
 async def hilfe(ctx):
     await ctx.send('''Tož co chceš...
-Ping - Vypíše ping.
+Ping - Vypíše ping bota.
 Beh dráha čas - zapíše běh.
 Na dalších commandech se pracuje. :)
     ''')
-client.run("TOKEN_HERE")
+client.run("NzA2OTI5NTkwNDA4NDQ1OTU1.XrJ6Vg.RkK7HxF7_QgWQZ-PQU7eFFopVBc")
 
